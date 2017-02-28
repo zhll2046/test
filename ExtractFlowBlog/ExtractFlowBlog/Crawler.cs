652eb7a6bdd1db77bc00d909746cdc3e9ab96956
v-lvzhan <v-lvzhan@ERICZHANG>
2017-02-28 17:59:40 +0800

    a new application

---
+++ ExtractFlowBlog/ExtractFlowBlog/Crawler.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractFlowBlog
+{
+    class Crawler
+    {
+
+        public static HtmlDocument getHtmlDoc(string url) {
+
+            HtmlWeb page = new HtmlWeb();
+            HtmlDocument document = page.Load(url);
+            return document;
+
+        }
+
+
+
+    }
+}
---
+++ ExtractFlowBlog/ExtractFlowBlog/Program.cs
@@ -0,0 +1,61 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractFlowBlog
+{
+
+    class Program
+    {
+        public static string root = @"https://flow.microsoft.com";
+        static void Main(string[] args)
+        {
+
+            GetAllBlogs();
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> GetAllBlogs()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            int page = 1;
+
+            while (true)
+            {
+
+                string url = @"https://flow.microsoft.com/en-us/blog/?page=" + page;
+                HtmlDocument hdoc = Crawler.getHtmlDoc(url);
+                HtmlNode node = hdoc.GetElementbyId("blog-results");
+
+                if (node.SelectNodes("div/h3")?[0]?.InnerHtml != "No blog posts were found.")
+                {
+
+                    foreach (HtmlNode childnode in node.SelectNodes("div[contains(@class,'row column')]"))
+                    {
+
+                        HtmlNode targetNode = childnode.SelectNodes("article/div/h2/a")[0];
+
+                        dic.Add(root + targetNode.GetAttributeValue("href", ""), targetNode.InnerHtml);
+                         
+                    }
+
+                    page++;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("No blog posts were found");
+                    break;
+                }
+            }
+
+            return dic;
+
+        }
+    }
+}
