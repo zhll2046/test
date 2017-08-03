a9b64ef1ebe2a82eca0ef5df5b45e321d28ae72a
EricZhang271 <v-lvzhan@microsoft.com>
2017-08-03 22:24:43 +0800

    DDD

--- extractCAToolTags/extractCAToolTags/Program.cs
+++ extractCAToolTags/extractCAToolTags/Program.cs
@@ -17,21 +17,47 @@ using System.Threading.Tasks;
 namespace extractCAToolTags
 {
 
-    
+
     class Program
     {
 
         static NameValueCollection nvc = ConfigurationManager.AppSettings;
+        static string sql = @"WITH cte " +
+           "AS (SELECT *, " +
+      "'https://cas-portal-uat-api.azurewebsites.net/api/v2.0/instances?product=' " +
+      "+ product + '&platform=' + [platform] " +
+      "+ '&queue=&date={\"startDate\":\"' + startdate " +
+      "+ '\",\"endDate\":\"' + enddate + '\"}&code=' + id requestURL," +
+      "Row_number() " +
+      "  OVER(" +
+      "    partition BY product, platform, startdate, enddate, level1node," +
+      "  level2node" +
+      "    ORDER BY indate DESC)   rn," +
+      " id % 8[GROUP]" +
+      " FROM   tagssumarizedtable) " +
+      "SELECT *" +
+      "FROM   cte " +
+      "WHERE  rn = 1 " +
+      "ORDER BY [group] ";
 
-
-        static string ConnectionString = @"Server=wx-sql-test-01;Database=UserVoiceDB;User Id=sa;Password = sa;";
-        static DateTime startDate_ = new DateTime(2017,1,1);
+        static string ConnectionString = @"Server=horo;Database=UserVoiceDB;User Id=sa;Password = Sjynige2b;";
+        static DateTime startDate_ = new DateTime(2017, 1, 1);
 
         //static string endDate = ConfigurationManager.AppSettings["endDate"]; 
 
         static void Main(string[] args)
         {
 
+            //loadDataToTagsSummizedTable();
+
+            getThreadInfo(null);
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static void loadDataToTagsSummizedTable()
+        {
             Parallel.ForEach(nvc.AllKeys, (key) =>
             {
                 DateTime startDateInLoop = startDate_;
@@ -56,14 +82,10 @@ namespace extractCAToolTags
                     startDateInLoop = startDateInLoop.AddMonths(1);
 
 
-                } while (startDateInLoop < DateTime.Now);  
+                } while (startDateInLoop < DateTime.Now);
 
             });
-             
-            Console.WriteLine("Done");
-            Console.ReadKey();
         }
-
 
         static DataTable GetSummarizedTable()
         {
@@ -110,16 +132,16 @@ namespace extractCAToolTags
             //this.dataTable.Clear();
         }
 
-        static IList<Parent> getProductInfo(string product, string platform, string startDate , string endDate)
+        static IList<Parent> getProductInfo(string product, string platform, string startDate, string endDate)
         {
 
-            HttpWebRequest request = System.Net.HttpWebRequest.CreateHttp("http://cas-portal-uat-api.azurewebsites.net/api/v1.0/products/"+product+"/tree?platform="+platform+"&queueId=&date={%22startDate%22:%22"+startDate+"%22,%22endDate%22:%22"+endDate+"%22}");
+            HttpWebRequest request = System.Net.HttpWebRequest.CreateHttp("http://cas-portal-uat-api.azurewebsites.net/api/v1.0/products/" + product + "/tree?platform=" + platform + "&queueId=&date={%22startDate%22:%22" + startDate + "%22,%22endDate%22:%22" + endDate + "%22}");
             //POST web request to create a datasource.
             request.KeepAlive = true;
             request.Method = "GET";
             request.ContentLength = 0;
 
-            IList<Parent> responseJson=null;
+            IList<Parent> responseJson = null;
 
             //Write JSON byte[] into a Stream
             using (var response = (HttpWebResponse)request.GetResponse())
@@ -127,15 +149,75 @@ namespace extractCAToolTags
 
                 var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
                 responseJson = JsonConvert.DeserializeObject<IList<Parent>>(responseString);
-                 
+
             }
             return responseJson;
         }
-         
+        static IList<Thread> getThreadInfo(String url)
+        {
+
+            //HttpWebRequest request = System.Net.HttpWebRequest.CreateHttp("http://cas-portal-uat-api.azurewebsites.net/api/v1.0/products/" + product + "/tree?platform=" + platform + "&queueId=&date={%22startDate%22:%22" + startDate + "%22,%22endDate%22:%22" + endDate + "%22}");
+            HttpWebRequest request = System.Net.HttpWebRequest.CreateHttp("https://cas-portal-uat-api.azurewebsites.net/api/v2.0/instances?product=powerbi&platform=powerbi &queue=&date={\"startDate\":\"2017-01-01T00:00:00Z\",\"endDate\":\"2017-02-01T00:00:00Z\"}&code=380");
+            //POST web request to create a datasource.
+            request.KeepAlive = true;
+            request.Method = "GET";
+            request.ContentLength = 0;
+
+            IList<Thread> responseJson = null;
+
+            //Write JSON byte[] into a Stream
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                responseJson = JsonConvert.DeserializeObject<IList<Thread>>(responseString);
+
+            }
+            return responseJson;
+        }
+
 
     }
 
-    class Child {
+
+    class Thread
+    {
+
+        [JsonProperty("id")]
+        public string id { set; get; }
+
+        [JsonProperty("orignalQuestionId")]
+        public string orignalQuestionId { set; get; }
+
+        [JsonProperty("title")]
+        public string title { set; get; }
+
+        [JsonProperty("url")]
+        public string url { set; get; }
+
+        [JsonProperty("createdOn")]
+        public string createdOn { set; get; }
+
+        [JsonProperty("sentiment")]
+        public string sentiment { set; get; }
+
+        [JsonProperty("queue")]
+        public Queue queue { set; get; }
+    }
+
+    class Queue
+    {
+        [JsonProperty("id")]
+        public string id { set; get; }
+        [JsonProperty("name")]
+        public string name { set; get; }
+        [JsonProperty("displayname")]
+        public string displayname { set; get; }
+
+    }
+
+    class Child
+    {
 
         [JsonProperty("id")]
         public string id { get; set; }
@@ -145,7 +227,8 @@ namespace extractCAToolTags
         public string data { get; set; }
     }
 
-    class Parent {
+    class Parent
+    {
         [JsonProperty("id")]
         public string id { get; set; }
         [JsonProperty("name")]
@@ -154,7 +237,7 @@ namespace extractCAToolTags
         public string data { get; set; }
 
         [JsonProperty("children")]
-       public IList<Child> children { get; set; }
+        public IList<Child> children { get; set; }
     }
- 
+
 }
